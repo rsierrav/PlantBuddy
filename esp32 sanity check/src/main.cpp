a9e395388ff17c8464b12c8c701cb9dfce702c83
rsierrav <rosagloriasv27@gmail.com>
2025-10-03 19:22:33 -0400

    This had lcd starter code

--- esp32 sanity check/src/main.cpp
+++ esp32 sanity check/src/main.cpp
@@ -1,15 +1,36 @@
+// this tests the LiquidCrystal_I2C library with an ESP32
+
 #include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+// Try 0x27 first, some modules use 0x3F
+#define LCD_ADDR 0x27  
+
+LiquidCrystal_I2C lcd(LCD_ADDR, 16, 2);
 
 void setup() {
-  Serial.begin(115200);   // start serial monitor
-  pinMode(2, OUTPUT);     // onboard LED is on GPIO 2
+  Wire.begin(21, 22); // SDA = 21, SCL = 22
+  lcd.init();
+  lcd.backlight();
+
+  lcd.setCursor(0, 0);
+  lcd.print("Plant Buddy");
+  lcd.setCursor(0, 1);
+  lcd.print("LCD Hello!");
 }
 
 void loop() {
-  digitalWrite(2, HIGH);
-  Serial.println("LED ON");
+  // Rotate messages
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Hello ESP32!");
   delay(1000);
-  digitalWrite(2, LOW);
-  Serial.println("LED OFF");
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Rosa and Es's ");
+  lcd.setCursor(0, 1);
+  lcd.print("Project");
   delay(1000);
 }
